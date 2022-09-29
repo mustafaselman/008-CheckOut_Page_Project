cece6549790d6d9fb7caa2e879bdd0ade2ded5c2
mustafaselmanonal <mustafaselman.100@gmail.com>
2022-09-29 15:32:01 +0300

    upgrade

--- app.js
+++ app.js
@@ -16,62 +16,48 @@ let productTotal3 = document.querySelector("#productTotal3");
 let subtotal = document.querySelector("#subtotal");
 let tax = document.querySelector("#tax");
 let total = document.querySelector("#total");
-
+let container = document.querySelector(".container");
 
 eventListeners();
-function eventListeners()
+function eventListeners(e)
 {
-    buttonDecrease1.addEventListener("click", () =>
+
+    container.addEventListener("click", (e) =>
     {
-        decreaseProductTotal(productTotal1, quantity1);
-        decrease(quantity1);
-    });
-    buttonIncrease1.addEventListener("click", () =>
-    {   
-        increaseProductTotal(productTotal1, quantity1);
-        increase(quantity1)
-    });
-    buttonDecrease2.addEventListener("click", () =>
-    {
-        decreaseProductTotal(productTotal2, quantity2);
-        decrease(quantity2);
-    });
-    buttonIncrease2.addEventListener("click", () =>
-    {   
-        increaseProductTotal(productTotal2, quantity2);
-        increase(quantity2);
-    });
-    buttonDecrease3.addEventListener("click", () =>
-    {
-        decreaseProductTotal(productTotal3, quantity3);
-        decrease(quantity3);
-    });
-    buttonIncrease3.addEventListener("click", () =>
-    {   
-        increaseProductTotal(productTotal3, quantity3);
-        increase(quantity3);
-    });
-    remove1.addEventListener("click", () =>
-    {
-        removeFunction(remove1,productTotal1);
-        subtotalFunction();
-        taxFunction();
-        totalFunction();
-        
-    });
-    remove2.addEventListener("click", () =>
-    {
-        removeFunction(remove2,productTotal2);
-        subtotalFunction();
-        taxFunction();
-        totalFunction();
-    });
-    remove3.addEventListener("click", () =>
-    {
-        removeFunction(remove3,productTotal3);
-        subtotalFunction();
-        taxFunction();
-        totalFunction();
+        if (e.target.className.includes("buttonDecrease"))
+        {
+            console.log(e.target.className);
+            console.log(
+                e.target.parentElement.nextElementSibling.nextElementSibling.childNodes[ 1 ]
+            );
+            console.log(e.target.nextElementSibling);
+            decreaseProductTotal(
+                e.target.parentElement.nextElementSibling.nextElementSibling.childNodes[ 1 ],
+                e.target.nextElementSibling
+            );
+            decrease(e.target.nextElementSibling);
+        } else if (e.target.className.includes("buttonIncrease"))
+        {
+            console.log(
+                e.target.parentElement.nextElementSibling.nextElementSibling.childNodes[ 1 ],
+                e.target.previousElementSibling
+            )
+            increaseProductTotal(
+                e.target.parentElement.nextElementSibling.nextElementSibling.childNodes[ 1 ],
+                e.target.previousElementSibling
+            );
+            increase(e.target.previousElementSibling)
+        } else if (e.target.className.includes("remove"))
+        {
+            console.log(e.target, e.target.nextElementSibling.childNodes[ 1 ])
+            removeFunction(e.target, e.target.nextElementSibling.childNodes[ 1 ]);
+            subtotalFunction();
+            taxFunction();
+            totalFunction();
+
+
+        }
+
     });
 }
 
@@ -92,9 +78,11 @@ function decreaseProductTotal(productTotal, quantity)
 {
     if (Number(quantity.innerText) > 1)
     {
-        let price = Number(productTotal.innerText) / Number(quantity.innerText) * (Number(quantity.innerText) - 1) ;
+        let price = Number(productTotal.innerText) / Number(quantity.innerText) * (
+            Number(quantity.innerText) - 1
+        );
         productTotal.innerText = price.toFixed(2);
-        
+
     }
     subtotalFunction();
     taxFunction();
@@ -103,30 +91,39 @@ function decreaseProductTotal(productTotal, quantity)
 
 function increaseProductTotal(productTotal, quantity)
 {
-    let price = Number(productTotal.innerText) / Number(quantity.innerText) * (Number(quantity.innerText)+ 1);
+    let price = Number(productTotal.innerText) / Number(quantity.innerText) * (
+        Number(quantity.innerText) + 1
+    );
     productTotal.innerText = price.toFixed(2);
     subtotalFunction();
     taxFunction();
     totalFunction();
 }
 
-function subtotalFunction(){
-    let price = Number(productTotal1.innerText) + Number(productTotal2.innerText)  + Number(productTotal3.innerText)
+function subtotalFunction()
+{
+    let price = Number(productTotal1.innerText) + Number(productTotal2.innerText) +
+        Number(productTotal3.innerText)
     subtotal.innerText = price.toFixed(2);
 }
 
-function taxFunction() {
-let price = Number(subtotal.innerText) * 0.18;
-tax.innerText = price.toFixed(2); 
+function taxFunction()
+{
+    let price = Number(subtotal.innerText) * 0.18;
+    tax.innerText = price.toFixed(2);
 }
 
-function totalFunction () {
-let price = Number(subtotal.innerText) + Number(tax.innerText) + 15;
-total.innerText = price.toFixed(2); 
+function totalFunction()
+{
+    let price = Number(subtotal.innerText) + Number(tax.innerText) + 15;
+    total.innerText = price.toFixed(2);
 }
 
-function removeFunction (remove,productTotal) {
-    remove.parentElement.parentElement.remove();
+function removeFunction(remove, productTotal)
+{
+    remove
+        .parentElement
+        .parentElement
+        .remove();
     productTotal.innerText = 0;
 }
-
